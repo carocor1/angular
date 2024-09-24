bd79609070e7bf054513092e7e547e851b78d2bd
Kevin Borgo <kevinborgodvs@gmail.com>
2024-09-24 11:24:41 -0300

    pruebas en products.component.spec.ts

--- src/app/template/products/products.component.spec.ts
+++ src/app/template/products/products.component.spec.ts
@@ -1,23 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { ProductsService } from './products.service';
+import axios from 'axios';
+import { ProductI } from '../../interfaces/product';
 
-import { ProductComponent } from './products.component';
+describe('ProductsService', () => {
+  let service: ProductsService;
+  let axiosGetSpy: jasmine.Spy;
 
-describe('ProductComponent', () => {
-  let component: ProductComponent;
-  let fixture: ComponentFixture<ProductComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ProductsService]
+    });
+    service = TestBed.inject(ProductsService);
+                                                                                                 // Simular el localStorage para que tenga un token válido
+    spyOn(localStorage, 'getItem').and.returnValue(JSON.stringify({
+      accessToken: 'mockAccessToken'
+      }));
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProductComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ProductComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    axiosGetSpy = spyOn(axios, 'get');                                                             // Spy para simular la llamada a axios.get
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('debería obtener productos', async () => {
+    const mockProducts: ProductI[] = [
+      { name: 'Product 1', price: 100, productType: { id: 1, name: 'Type 1' } },
+      { name: 'Product 2', price: 200, productType: { id: 2, name: 'Type 2' } }
+    ];
+   
+    axiosGetSpy.and.returnValue(Promise.resolve({ data: mockProducts }));                       // Simulamos una respuesta exitosa sin hacer la petición real
+    const productos = await service.obtenerProductos();
+                                                                                                 // Verifica si se obtienen productos
+    expect(productos.length > 0).toBeTrue();
+  });
+
+  it('debería manejar correctamente la ausencia de productos', async () => {
+    const mockProducts: ProductI[] = []; // Array vacío, sin productos
+                                                                                                    // Simulamos una respuesta vacía sin productos
+    axiosGetSpy.and.returnValue(Promise.resolve({ data: mockProducts }));
+    const productos = await service.obtenerProductos();
+                                                                                                    // Verifica que no se obtienen productos (el array está vacío)
+    expect(productos.length === 0).toBeTrue();
   });
 });
